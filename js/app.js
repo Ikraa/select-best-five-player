63e0adf2c11f8c4bdef3303b13694c4c8e5e9f57
munnisaikra <munnisaikra750@gmail.com>
2022-08-22 03:19:13 +0600

    added player list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log("ikra")
+const getButton=document.getElementsByClassName("card-button")
+for(i=0;i<getButton.length;i++){
+    getButton[i].addEventListener('click',function(){
+        
+    })
+}
