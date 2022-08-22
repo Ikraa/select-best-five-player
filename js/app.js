9d14b4aa9a393810b0e7c20d878a54c60db687ae
munnisaikra <munnisaikra750@gmail.com>
2022-08-22 16:10:10 +0600

    gt player name

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-const getButton=document.getElementsByClassName("card-button")
-for(i=0;i<getButton.length;i++){
-    getButton[i].addEventListener('click',function(){
-        
-    })
+//get all button using class
+const getButton = document.getElementsByClassName("card-button");
+
+//loop for get single button 
+for (let i = 0; i < getButton.length; i++) {
+  const element = getButton[i];
+  element.addEventListener("click", function () {
+    const getParent = element.parentNode;
+   
+    const playerName= getParent.querySelector(".player-name").innerText;
+   
+   
+    
+  });
+  
+ 
 }
